2a24156b0361eda0b915a7b3ee171aadb1276c49
Martin Olson <martinguyolson@gmail.com>
2023-03-28 14:40:41 +1100

    updated spring? also started the update employee dto

--- EmployeeCreatorBackend2/employeecreator/src/main/java/com/martin/employeecreator/UpdateEmployeeDto.java
+++ EmployeeCreatorBackend2/employeecreator/src/main/java/com/martin/employeecreator/UpdateEmployeeDto.java
@@ -1,5 +1,39 @@
 package com.martin.employeecreator;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Size;
 
-public class UpdateEmployeeDto {}
+public class UpdateEmployeeDto {
+
+  @Size(min = 1)
+  String firstName;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  @Size(min = 1)
+  String middleName;
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+  @Size(min = 1)
+  String lastName;
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
