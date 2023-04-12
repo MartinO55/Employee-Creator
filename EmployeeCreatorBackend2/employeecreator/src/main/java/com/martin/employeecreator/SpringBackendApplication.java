8d8ca80ec6b7cf854e76b136b597de4c554b9f29
Martin Olson <martinguyolson@gmail.com>
2023-04-12 13:27:17 +1000

    main.class because for some reason it was not added in the build

---
+++ EmployeeCreatorBackend2/employeecreator/src/main/java/com/martin/employeecreator/SpringBackendApplication.java
@@ -0,0 +1,11 @@
+package com.martin.employeecreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SpringBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBackendApplication.class, args);
+  }
+}
