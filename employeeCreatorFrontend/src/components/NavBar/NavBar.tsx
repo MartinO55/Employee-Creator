197bbd5b372b113871a1970a3c4cafcfa63efc72
Martin Olson <martinguyolson@gmail.com>
2023-05-01 09:45:33 +1000

    navbar links exist

--- employeeCreatorFrontend/src/components/NavBar/NavBar.tsx
+++ employeeCreatorFrontend/src/components/NavBar/NavBar.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from "./NavBar.module.scss";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/NewEmployee">Add an Employee</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
