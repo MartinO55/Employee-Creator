761a1446fbc7980eda3d2f013e109e7fbd6ed8a7
Martin Olson <martinguyolson@gmail.com>
2023-05-10 11:33:18 +1000

    added another test

--- EmployeeCreatorBackend2/employeecreator/src/test/java/com/martin/employeecreator/EmployeecreatorApplicationTests.java
+++ EmployeeCreatorBackend2/employeecreator/src/test/java/com/martin/employeecreator/EmployeecreatorApplicationTests.java
@@ -1,13 +1,22 @@
 package com.martin.employeecreator;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.martin.employeecreator.Employee.CreateEmployeeDto;
+import com.martin.employeecreator.Employee.EmployeeService;
 import com.martin.employeecreator.Employee.EmployeecreatorApplication;
-import java.io.Console;
+import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -21,13 +30,19 @@ class EmployeecreatorApplicationTests {
 
   private MockMvc mockMvc;
 
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private EmployeeService employeeService;
+
   @BeforeEach
   public void setUp() {
     mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
   }
 
   @Test
-  public void testGetUsers() throws Exception {
+  public void testTest() throws Exception {
     mockMvc
       .perform(
         MockMvcRequestBuilders
@@ -35,7 +50,39 @@ class EmployeecreatorApplicationTests {
           .accept(MediaType.APPLICATION_JSON)
       )
       .andExpect(MockMvcResultMatchers.status().isOk());
+  }
 
-    System.out.println("test ran");
+  @Test
+  public void createValidEmployeeTest() throws Exception {
+    String firstName = "Little";
+    String middleName = "Johny";
+    String lastName = "Testcase";
+    String email = "TestCase@Testcase.com";
+    String homeAddress = "1 Test Lane";
+    String phoneNumber = "83377778";
+    String contractType = "casual";
+    LocalDate startDate = LocalDate.of(2022, 4, 13);
+    LocalDate endDate = LocalDate.of(2022, 4, 14);
+
+    CreateEmployeeDto employee = new CreateEmployeeDto(
+      firstName,
+      middleName,
+      lastName,
+      email,
+      homeAddress,
+      phoneNumber,
+      contractType,
+      startDate,
+      endDate
+    );
+
+    mockMvc
+      .perform(
+        post("/employees")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(objectMapper.writeValueAsString(employee))
+      )
+      .andExpect(status().isCreated())
+      .andReturn();
   }
 }
