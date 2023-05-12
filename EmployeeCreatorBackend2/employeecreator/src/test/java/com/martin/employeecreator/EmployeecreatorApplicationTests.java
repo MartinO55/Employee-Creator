a447e35b55e8cb3db1ec6bc261ba9bc88307eac4
Martin Olson <martinguyolson@gmail.com>
2023-05-12 10:08:51 +1000

    updated spring because it was annoying me. added more tests

--- EmployeeCreatorBackend2/employeecreator/src/test/java/com/martin/employeecreator/EmployeecreatorApplicationTests.java
+++ EmployeeCreatorBackend2/employeecreator/src/test/java/com/martin/employeecreator/EmployeecreatorApplicationTests.java
@@ -1,6 +1,8 @@
 package com.martin.employeecreator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -8,11 +10,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.martin.employeecreator.Employee.CreateEmployeeDto;
+import com.martin.employeecreator.Employee.Employee;
+import com.martin.employeecreator.Employee.EmployeeController;
 import com.martin.employeecreator.Employee.EmployeeService;
 import com.martin.employeecreator.Employee.EmployeecreatorApplication;
 import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy.Content;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +36,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(classes = EmployeecreatorApplication.class)
+@ExtendWith(MockitoExtension.class)
 class EmployeecreatorApplicationTests {
 
   @Autowired
@@ -39,6 +49,9 @@ class EmployeecreatorApplicationTests {
 
   @MockBean
   private EmployeeService employeeService;
+
+  @InjectMocks
+  private EmployeeController employeeController;
 
   @BeforeEach
   public void setUp() {
@@ -101,5 +114,31 @@ class EmployeecreatorApplicationTests {
       .perform(MockMvcRequestBuilders.get("/employees"))
       .andExpect(status().isOk())
       .andExpect(content().json("[]"));
+  }
+
+  @Test
+  public void GetByIDTest() throws Exception {
+    Employee mockEmployee = new Employee();
+
+    mockEmployee.setId(1L);
+    mockEmployee.setFirstName("Little");
+    mockEmployee.setMiddleName("Johnny");
+    mockEmployee.setLastName("Tester");
+    mockEmployee.setEmail("LittleJohnny@gmail.com");
+    mockEmployee.setHomeAddress("1 Test Street");
+    mockEmployee.setPhoneNumber("555555555");
+    mockEmployee.setContractType("Casual");
+
+    mockEmployee.setStartDate(LocalDate.of(2022, 4, 13));
+    mockEmployee.setEndDate(LocalDate.of(2022, 4, 14));
+
+    when(employeeService.findById(1L)).thenReturn(Optional.of(mockEmployee));
+
+    mockMvc
+      .perform(MockMvcRequestBuilders.get("/employees/1"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.id").value(1))
+      .andExpect(jsonPath("$.firstName").value("Little"))
+      .andExpect(jsonPath("$.contractType").value("Casual"));
   }
 }
